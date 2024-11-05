d029e49081c2bb1a0d5475e64819d7e7f3e89cda
majadi <majadi@fsdhgroup.com>
2024-11-05 05:34:01 -0800

    done

--- src/components/bars/sidebar.tsx
+++ src/components/bars/sidebar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import style from './sidebar.module.css';
 import { Button } from "react-bootstrap";
 
-const SideBarUnAuth: React.FC<any> = ({ onSideBar, toggleSideBar, onLogin }) => {
+const SideBarUnAuth: React.FC<any> = ({ onSideBar, onLogin }) => {
 
     const links = [
         { title: 'Go-pro', path: '' },
@@ -12,7 +12,7 @@ const SideBarUnAuth: React.FC<any> = ({ onSideBar, toggleSideBar, onLogin }) => 
         <div className={onSideBar ? `w-100 bg-primary flex-column ${style.onSidebar}` : `${style.offSidebar}`}
         >
             <div className="w-100 py-3 text-light  text-end px-3 fs-4">
-                <i className="bi bi-x-circle" role="button" onClick={() => toggleSideBar()}></i>
+                <i className="bi bi-x-circle" role="button"></i>
             </div>
             <ul className="d-flex flex-column align-items-center m-0 p-0 w-100 gap-2">
                 {
